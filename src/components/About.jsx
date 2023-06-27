4a2a1469298504e4fddf2a5480f66b4c23880eda
Sparsh <res.sparsh@gmail.com>
2023-06-28 00:01:44 +0530

    mypotfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-full bg-gradient-to-b from-gray-800 to-black text-white mt-[16rem] sm:mt-1"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full  lg:h-screen">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-4">
+          I am a third-year bachelor of computer science student from the KIET
+          group of institutions. I have gained valuable experience in developing
+          and maintaining software applications. I have a strong understanding
+          of programming languages in C++, fronted web development, and
+          methodologies in web development. My technical skills are complemented
+          by excellent problem-solving abilities and strong teamwork and
+          communication skills. I am always open to leveraging my technical
+          expertise and problem-solving abilities in a challenging software
+          engineering role. I am seeking a position that allows me to contribute
+          to the development of high-quality
+        </p>
+
+        <br />
+
+      
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import CryptoDetective from "../assets/portfolio/CryptoDetective.jpg";
+
+import navbar from "../assets/portfolio/navbar.jpg";
+
+import TextAnalyzer from "../assets/portfolio/TextAnalyzer.jpg";
+import NewsXpress from "../assets/portfolio/NewsXpress.jpg";
+import Web3NobilityPublicFunding from "../assets/portfolio/Web3NobilityPublicFunding.jpg"
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: CryptoDetective,
+      name:"CryptoDetective"
+    },
+    {
+      id: 2,
+      src: Web3NobilityPublicFunding,
+      name:"Web3NobilityPublicFunding"
+    },
+    {
+      id: 3,
+      src: NewsXpress,
+      name:"NewsXpress"
+    },
+    {
+      id: 4,
+      src: navbar,
+      name:"Navbar"
+    },
+    {
+      id: 5,
+      src: TextAnalyzer,
+      name:"TextAnalyzer"
+    },
+    {
+      id: 6,
+      src: "",
+      name:"Upcoming "
+    },
+  ];
+
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full h-full lg:h-screen text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          
+          {portfolios.map(({ id, src ,name}) => (
+            
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+            <h1 className="mb-2 font-extrabold">{name} </h1>
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className=" flex items-center justify-center ">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
+              </div>
+            </div>
+            
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
